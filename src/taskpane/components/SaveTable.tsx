b1b98c6132b0f392f3a1075a93fda9d2a25e8af1
Abdul Wahab <abdul.wahab@softpyramid.com>
2020-12-09 21:06:53 +0500

    save table modal

--- src/taskpane/components/SaveTable.tsx
+++ src/taskpane/components/SaveTable.tsx
@@ -1,46 +1,59 @@
 import * as React from "react";
-import { Checkbox, CompoundButton, DefaultButton, Dialog, DialogFooter, DialogType, PrimaryButton, TextField} from 'office-ui-fabric-react';
-// import { useBoolean } from "@uifabric/react-hooks";
+import { Checkbox, CompoundButton, DefaultButton, Dialog, DialogFooter, DialogType, IStackTokens, PrimaryButton, Stack, TextField} from 'office-ui-fabric-react';
 
-// const [hideDialog, { toggle: toggleHideDialog }] = useBoolean(true);
+
+// type: DialogType.largeHeader,
 const dialogContentProps = {
-  type: DialogType.largeHeader,
   title: 'Save Table Style',
+};
+
+const verticalGapStackTokens: IStackTokens = {
+  childrenGap: 10,
+  padding: 10,
 };
 export interface AppState {
   hideDialog: boolean;
 }
+export interface AppProps {
+}
 
-export default class SaveTable extends React.Component<{}, AppState> {
+export default class SaveTable extends React.Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
       hideDialog: true,
     }
   }
-
   toggleHideDialog() {
-    this.setState({hideDialog: !this.state.hideDialog})
+    this.setState({hideDialog: !this.state.hideDialog});
   }
+
   render() {    
     return (
       <div>
-        <CompoundButton primary secondaryText="Sub Text" onClick={this.toggleHideDialog} >
-          Save Table Style
-        </CompoundButton>
+        <Stack>
+          <Stack.Item align="center">
+
+          <CompoundButton primary secondaryText="Sub Text" onClick={() => this.toggleHideDialog()} >
+            Save Table Style
+          </CompoundButton>
+          </Stack.Item>
+        </Stack>
 
       <Dialog
         hidden={this.state.hideDialog}
-        onDismiss={this.toggleHideDialog}
+        onDismiss={() => this.toggleHideDialog()}
         dialogContentProps={dialogContentProps}
       >
-        <TextField label="Table Style Name" placeholder="Table Name" />
-        <Checkbox label="Set as default table style" />
-        <Checkbox label="Replace default cell styles" />
+        <Stack  tokens={verticalGapStackTokens}>
+          <TextField label="Table Style Name" placeholder="Table Name" />
+          <Checkbox label="Set as default table style" />
+          <Checkbox label="Replace default cell styles" />
+        </Stack>
 
         <DialogFooter>
-          <PrimaryButton onClick={this.toggleHideDialog} text="Cancel" />
-          <DefaultButton onClick={this.toggleHideDialog} text="Save" />
+          <PrimaryButton onClick={() => this.toggleHideDialog()} text="Cancel" />
+          <DefaultButton onClick={() => this.toggleHideDialog()} text="Save" />
         </DialogFooter>
       </Dialog>
       </div>
